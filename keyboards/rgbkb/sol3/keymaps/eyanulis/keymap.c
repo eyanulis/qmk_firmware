af1ecfccf505f847baea68f47a6dcaafcde752f3
Eric M. Yanulis <eric@eyanulis.net>
2024-01-28 22:54:01 -0700

    rgbkb/sol3/eyanulis: Move RGB_MOD on ADJ layer; swap thumb DEL to FN

--- keyboards/rgbkb/sol3/keymaps/eyanulis/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/eyanulis/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │Shift │   Z  │   X  │   C  │   V  │   B  │   {  │  │   }  │   N  │   M  │   ,  │   .  │   /  │Shift │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Ctrl │ Meta │  Alt │  ADJ │ Meta │      │ DEL  │  │ Enter│      │ Meta │ Left │ Down │ Up   │Right │
+ * │ Ctrl │ Meta │  Alt │  ADJ │ Meta │      │ FN   │  │ Enter│      │ Meta │ Left │ Down │ Up   │Right │
  * └──────┴──────┴──────┴──────┴──────┤ Space├──────┤  ├──────┤ Space├──────┴──────┴──────┴──────┴──────┘
  *                                    │      │ FN   │  │ Enter│      │
  *                                    └──────┴──────┘  └──────┴──────┘
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,                  KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR,                  KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL,  KC_LGUI, KC_LALT, ADJUST,  KC_LGUI, KC_SPC,  KC_DEL,  FN,     KC_ENT,  KC_ENT,  KC_SPC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        KC_LCTL,  KC_LGUI, KC_LALT, ADJUST,  KC_LGUI, KC_SPC,  FN,      FN,     KC_ENT,  KC_ENT,  KC_SPC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
 
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │      │ SPD  │      │ SPI  │      │      │      │  │      │      │ Num1 │ Num2 │ Num3 │      │      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │      │ RMOD │ TOG  │ MOD  │      │      │      │  │      │      │ Num0 │NumDot│ NumLk│      │      │
+ * │      │ RMOD │ TOG  │ (adj)│ MOD  │      │      │  │      │      │ Num0 │NumDot│ NumLk│      │      │
  * └──────┴──────┴──────┴──────┴──────┤      ├──────┤  ├──────┤      ├──────┴──────┴──────┴──────┴──────┘
  *                                    │      │      │  │      │      │
  *                                    └──────┴──────┘  └──────┴──────┘
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_SAD, RGB_VAI, RGB_SAI, _______, _______, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
         _______, RGB_HUD, RGB_VAD, RGB_HUI, RGB_RST, _______, _______,                   _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
         _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______,                   _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
-        _______, RGB_RMOD,RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NUM,  _______, _______,
+        _______, RGB_RMOD,RGB_TOG, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NUM,  _______, _______,
 
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
